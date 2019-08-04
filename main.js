2dbbf1c43acbf55f7d63d325f2b56580782e1720
OHovey <30669691+OHovey@users.noreply.github.com>
2019-08-04 02:11:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -60,6 +60,14 @@ function createWindow () {
       ],
     },
     {
+      label: 'Edit',
+      submenu: [
+        { label: 'Cut', accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      ]
+    },
+    {
       label: 'Help',
       submenu: [
         { label: 'Launch Dev Tools', click() {
